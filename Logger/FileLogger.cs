00e45b4635f5e9662bc19b7f53e47f92dc27a437
lsobtafo <69612582+lsobtafo@users.noreply.github.com>
2023-01-26 21:10:37 -0800

    Assignment2 latest update

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+using System;
+namespace Logger
+{
+    public class FileLogger : BaseLogger
+    {
+        private string _FilePath { get; }
+        public FileLogger(string filePath) { _FilePath = filePath; }
+
+        public override void Log(LogLevel logLevel, string message)
+        {
+            if (!string.IsNullOrEmpty(_FilePath))
+            {
+                File.AppendAllText(_FilePath, $"{DateTime.Now}{ClassName}{logLevel + ":"} {message}\n");
+
+            }
+            else
+            {
+                throw new ArgumentException("the file path is empty!");
+            }
+        }
+    }
+}
+
